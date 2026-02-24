e80dc2ced2bc607ddb0de4a683795bbcc9ba1d08
Codename1001 <88858837+Codename1001@users.noreply.github.com>
2026-02-24 00:48:41 -0500

    updated ui

--- scenes/components/hud.gd
+++ scenes/components/hud.gd
@@ -5,4 +5,4 @@ var data = { "speed" : 0, "health": 100, "ammo":0, "position": Vector2.ZERO}
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func update_hud_data():
-	data_display.text = "speed : " + str(data.speed) + "m/s" + "\nhealth: " +str(data.health)+ "% \nammo: " + str(data.ammo) + " Rnd's \nposition: \n(" +str(roundi(data.position.x)) + ", " + str(roundi(data.position.y))+ ")"
+	data_display.text = "speed : " + str(data.speed) + "m/s" + "\nhealth: " +str(data.health)+  "\nposition: \n(" +str(roundi(data.position.x)) + ", " + str(roundi(data.position.y))+ ")" + "\nammo: " + str(data.ammo) + " Rnd's"
