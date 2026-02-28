be2db1ee25a34f31de6095dbcca83a84522f3bc0
Codename1001 <88858837+Codename1001@users.noreply.github.com>
2026-02-27 22:11:25 -0500

    guns update v 1.1.0

--- scenes/components/guns.gd
+++ scenes/components/guns.gd
@@ -4,16 +4,23 @@ extends Node2D
 
 @onready var perminint_marker_scene = preload("res://scenes/components/permenint_marker.tscn")
 
+@onready var marker = $Marker2D
+
 func place_tracker():
 	var new_marker = perminint_marker_scene.instantiate()
-	new_marker.global_position = ray.get_collider().to_local(ray.get_collision_point())
-	if ray.get_collider().is_in_group("entity"):
-		new_marker.change_color("blue")
-	if ray.get_collider().is_in_group("enemy"):
-		new_marker.change_color("red")
-	if ray.get_collider().is_in_group("object"):
-		new_marker.change_color("green")
+	var distance = ray.get_collider().to_local(marker.global_position).distance_to(ray.get_collider().to_local(ray.get_collision_point()))
+	var duration = distance / 600
+	var object_type
+	new_marker.global_position = ray.get_collider().to_local(marker.global_position)
+	object_type = ray.get_collider()
+	#if ray.get_collider().is_in_group("entity"):
+		#color = "blue"
+	#if ray.get_collider().is_in_group("enemy"):
+		#color = "red"
+	#if ray.get_collider().is_in_group("object"):
+		#color = "green"
 	ray.get_collider().add_child(new_marker)
+	new_marker.move_marker(distance,ray.get_collider().to_local(ray.get_collision_point()), object_type)
 
 
 func _process(delta: float) -> void:
--- scenes/components/permenint_marker.gd
+++ scenes/components/permenint_marker.gd
@@ -4,15 +4,33 @@ extends Node2D
 
 var marker_colors = {"green":Color(0.369, 0.894, 0.106, 1.0),"red":Color(0.74, 0.074, 0.0),"blue":Color(0.0, 0.459, 0.735)}
 
+var target_hit = false
 
 func change_color(color:String):
 	modulate= marker_colors[color]
 
 
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func move_marker(distance,target, type):#,color):
+	if target_hit == false:
+		change_color("green")
+		var duration = distance / 600
+		var tween = create_tween()
+		tween.tween_property(self,"position",target,duration).set_trans(Tween.TRANS_LINEAR)
+		#new_marker.global_position = ray.get_collider().to_local(marker.position)        funny visual
+		await get_tree().create_timer(duration).timeout
+		tween = create_tween()
+		tween.tween_property(self,"global_scale",Vector2(5,5),.4).set_trans(Tween.TRANS_LINEAR)
+		await get_tree().create_timer(.5).timeout
+		tween = create_tween()
+		tween.tween_property(self,"global_scale",Vector2(1,1),.4).set_trans(Tween.TRANS_LINEAR)
+		target_hit = true
+
+		if type.is_in_group("entity"):
+			change_color("blue")
+		if type.is_in_group("enemy"):
+			change_color("red")
+
+	else:
+		pass
