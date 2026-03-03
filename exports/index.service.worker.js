8b9dca5f93fc4e2d8936ae95b33b2aeff0c1cfbe
Codename1001 <88858837+Codename1001@users.noreply.github.com>
2026-03-02 22:58:35 -0500

    testing updating system v1.3.3

--- exports/index.service.worker.js
+++ exports/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772510179|27894937645';
+const CACHE_VERSION = '1772510292|28007276181';
 /** @type {string} */
 const CACHE_PREFIX = 'radar-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
