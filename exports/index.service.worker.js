33777fa64481907274604689e6369755d5fee1f0
Codename1001 <88858837+Codename1001@users.noreply.github.com>
2026-03-30 23:09:23 -0400

    atempting to update website

--- exports/index.service.worker.js
+++ exports/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772511256|28971234603';
+const CACHE_VERSION = '1774926537|20895388033';
 /** @type {string} */
 const CACHE_PREFIX = 'radar-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
